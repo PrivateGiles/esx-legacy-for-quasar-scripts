501fcdb88be953291fe6354add067b0c2819e0b0
PrivateGiles <56416493+PrivateGiles@users.noreply.github.com>
2022-12-01 19:40:30 +0000

    Add files via upload

---
+++ [core]/skinchanger/fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Skin Changer'
+
+version '1.0.3'
+
+lua54 'yes'
+
+escrow_ignore {
+	'locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/tr.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+client_scripts {
+	'locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/tr.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+dependency '/assetpacks'
